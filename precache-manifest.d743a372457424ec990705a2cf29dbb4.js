937a7fbefd70f6c68b76cf8d1b73b20913e78d06
Bushra-B <bushraxbil@gmail.com>
2020-07-04 17:38:12 +0000

    Updates

---
+++ precache-manifest.d743a372457424ec990705a2cf29dbb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96c36c5b9e5b4d643064025df8aec09",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "fd8de48326a93ff009d3",
+    "url": "/resty/static/css/main.21b6a018.chunk.css"
+  },
+  {
+    "revision": "f1cd1749ed1f121ebb6e",
+    "url": "/resty/static/js/2.75a41c79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.75a41c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8de48326a93ff009d3",
+    "url": "/resty/static/js/main.4f7ab2ab.chunk.js"
+  },
+  {
+    "revision": "6972438e44fdcad64bce",
+    "url": "/resty/static/js/runtime-main.b658189f.js"
+  }
+]);
